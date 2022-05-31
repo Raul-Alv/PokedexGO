a6e48510619353487d1fc6fc5395366ad6623def
UO265210 <uo265210@uniovi.es>
2022-05-31 23:09:18 +0200

    first commit

---
+++ inicioSesion.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>Inicie Sesión</h1>
+        <form class="formulario" method="post">
+
+            <label id="labelEmail" for="email">Email</label><input type="email" id="email"><br>
+            <label id="labelPassword" for="password">Contraseña</label><input type="password" id="password"><br>
+
+            <input id="enviar" type="submit" value="Enviar"><br>
+        </form>
+    </body>
+</html>
